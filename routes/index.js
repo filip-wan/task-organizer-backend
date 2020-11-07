2585048395467abfd2ebed31fd42b207d2ac233e
Filip Wanat <filip.k.wanat@gmail.com>
2020-11-07 16:12:14 +0100

    🎨 Refactor routes

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,13 @@
-import passport from 'passport';
 import express from 'express';
+import routerAuthorization from './routerAuthorization.js';
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const { user } = req;
-  res.status(200).send({ body: req.sessionID, user });
+  res.status(200).send({ sessionID: req.sessionID, user });
 });
 
-router.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile'] })
-);
-
-router.get('/auth/facebook', passport.authenticate('facebook'));
-
-router.get(
-  '/auth/github',
-  passport.authenticate('github', {
-    scope: ['user:email'],
-  })
-);
-
-router.get('/logout', (req, res, next) => {
-  req.logout();
-  res.redirect('/');
-});
-
-router.get(
-  '/auth/google/redirect',
-  passport.authenticate('google', { failureRedirect: '/' }),
-  (req, res, next) => {
-    res.redirect('/');
-  }
-);
-router.get(
-  '/auth/facebook/redirect',
-  passport.authenticate('facebook', { failureRedirect: '/' }),
-  (req, res, next) => {
-    res.redirect('/');
-  }
-);
-router.get(
-  '/auth/github/redirect',
-  passport.authenticate('github', { failureRedirect: '/' }),
-  (req, res, next) => {
-    res.redirect('/');
-  }
-);
+routerAuthorization(router);
 
 export default router;
---
+++ routes/routerAuthorization.js
@@ -0,0 +1,47 @@
+import passport from 'passport';
+
+const routerAuthorization = (router) => {
+  router.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile'] })
+  );
+
+  router.get('/auth/facebook', passport.authenticate('facebook'));
+
+  router.get(
+    '/auth/github',
+    passport.authenticate('github', {
+      scope: ['user:email'],
+    })
+  );
+
+  router.get(
+    '/auth/google/redirect',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (_req, res) => {
+      res.redirect('/');
+    }
+  );
+
+  router.get(
+    '/auth/facebook/redirect',
+    passport.authenticate('facebook', { failureRedirect: '/' }),
+    (_req, res) => {
+      res.redirect('/');
+    }
+  );
+  router.get(
+    '/auth/github/redirect',
+    passport.authenticate('github', { failureRedirect: '/' }),
+    (_req, res) => {
+      res.redirect('/');
+    }
+  );
+
+  router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+};
+
+export default routerAuthorization;
