e338bd8ade440198737af5916ea83684ca1523cb
Filip Wanat <filip.k.wanat@gmail.com>
2020-11-19 22:39:24 +0100

    ⚗ Add WEBSITE_URL_ORIGIN env variable

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import routes from './src/routes/index.js';
 import initializeAuthorization from './src/auth/index.js';
 import initializeDatabase from './src/db/index.js';
 
-const { SESSION_SECRET, PORT, WEBSITE_URL } = process.env;
+const { SESSION_SECRET, PORT, WEBSITE_URL, WEBSITE_URL_ORIGIN } = process.env;
 
 const app = express();
 const port = PORT ?? 3000;
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.enable('trust proxy');
 app.use(
   cors({
-    origin: WEBSITE_URL, // allow to server to accept request from different origin
+    origin: WEBSITE_URL_ORIGIN || WEBSITE_URL, // allow to server to accept request from different origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // allow session cookie from browser to pass through
   })
